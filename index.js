b5d3ba78cf0a9c72308173145cb924ccc001824e
Shubh0622 <shubhambachhav8@gmail.com>
2024-03-30 20:46:16 +0530

    successfully added functionality

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
 function handleFormSubmit(event){
-  event.preventDefault();
-  let myObj=JSON.parse(localStorage.getItem("expenseDetails")) || [];
-  myObj.push({
-    expenseAmount: event.target.expenseAmount.value,
-    description: event.target.description.value,
-    category: event.target.expenses.value
-  });
-//   let users= JSON.parse(localStorage.getItem(event.target.email.value))|| [];
-//   users.push(array);
-  localStorage.setItem("expenseDetails",JSON.stringify(myObj));
-let ul=document.querySelector("ul");
-let li =document.createElement("li");
-li.innerHTML=`${event.target.expenseAmount.value} - ${event.target.description.value} - ${event.target.expenses.value} <button type='button' class='delete-btn' data-des='${event.target.description.value}'>Delete Expenses</button> <button type='button' class='edit-btn' data-des='${event.target.description.value}'>Edit Expenses</button>`;
-li.id=event.target.description.value;
-ul.appendChild(li);
-const deleteBtn = document.querySelector(".delete-btn");
-  deleteBtn.addEventListener("click",function(event){
-    let desc = deleteBtn.getAttribute("data-des");
+    event.preventDefault();
+    let myObj=JSON.parse(localStorage.getItem("expenseDetails")) || [];
+    myObj.push({
+        expenseAmount: event.target.expenseAmount.value,
+        description: event.target.description.value,
+        category: event.target.expenses.value
+    });
+    localStorage.setItem("expenseDetails",JSON.stringify(myObj));
     let ul=document.querySelector("ul");
-    let li=document.getElementById(desc);
-    ul.removeChild(li);
-    //localStorage.removeItem(desc);
-  });
+    let li =document.createElement("li");
+    li.innerHTML=`${event.target.expenseAmount.value} - ${event.target.description.value} - ${event.target.expenses.value} <button type='button' class='delete-btn' data-des='${event.target.description.value}' onclick='handleDelete(event)'>Delete Expenses</button> <button type='button' class='edit-btn' data-des='${event.target.description.value}' onclick='handleEdit(event)'>Edit Expenses</button>`;
+    li.id=event.target.description.value;
+    ul.appendChild(li);
+    document.querySelector("form").reset();
 }
-//   const deleteBtn = document.querySelector(".delete-btn");
-//   deleteBtn.addEventListener("click",function(event){
-//     let desc = deleteBtn.getAttribute("data-des");
-//     console.log(desc);
-//     // const li = document.getElementById(desc);
-//     // li.remove();
-//     // localStorage.removeItem(desc);
-//   });
 
-//   const editBtn=document.querySelector(".edit-btn");
-//   editBtn.addEventListener("click",function(event){
-//     const email=editBtn.getAttribute("data-email");
-//     const getItem=JSON.parse(localStorage.getItem(email));
-//     document.getElementById("username").value=getItem.name;
-//     document.getElementById("email").value=getItem.email;
-//     document.getElementById("phone").value=getItem.phone;
-//     const li=document.getElementById(email);
-//     li.remove();
-//     localStorage.removeItem(email);
-//   })
+function handleDelete(event){
+    event.preventDefault();
+    let ulElem =document.querySelector("ul");
+    let desc =event.target.getAttribute("data-des");
+    //console.log(desc);
+    let liElem = document.getElementById(desc);
+    //console.log(liElem);
+    ulElem.removeChild(liElem);
+    let arr = JSON.parse(localStorage.getItem("expenseDetails"));
+    let elem=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].description===desc){
+            elem=i;
+            break;
+        }
+    }
+    arr.splice(elem,1);
+    localStorage.setItem("expenseDetails",JSON.stringify(arr));
+}
+    
+function handleEdit(event){
+    event.preventDefault();
+    let ulElem =document.querySelector("ul");
+    let desc =event.target.getAttribute("data-des");
+    let arr = JSON.parse(localStorage.getItem("expenseDetails"));
+    let elem=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].description===desc){
+            elem=i;
+            break;
+        }
+    }
+    document.getElementById("expA").value=arr[elem].expenseAmount;
+    document.getElementById("des").value=arr[elem].description;
+    document.getElementById("exp").value=arr[elem].category;
+    let liElem = document.getElementById(desc);
+    ulElem.removeChild(liElem);
+    arr.splice(elem,1);
+    localStorage.setItem("expenseDetails",JSON.stringify(arr));
+}
